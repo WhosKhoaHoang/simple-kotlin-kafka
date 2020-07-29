eb7949b85d926af7bfd366fcbac6facb6562bdfa
Khoa Hoang <whoskhoahoang@gmail.com>
2020-07-29 10:04:25 +0900

    Initial commit

---
+++ src/test/kotlin/com/example/simplekotlinkafka/SimpleKotlinKafkaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.simplekotlinkafka
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleKotlinKafkaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
